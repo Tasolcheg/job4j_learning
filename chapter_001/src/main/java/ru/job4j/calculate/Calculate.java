70898128d46b857b7bb22d68a8535b617365af0e
Kretov Alexandr <tasolcheg@gmail.com>
2018-06-03 14:06:20 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,23 @@
+/**
+* Package for calculate task.
+*
+* @author Kretov Alexandr (no mail)
+* @version $Id$
+* @since 0.1
+*/
 package ru.job4j.calculate;
 public class Calculate
 {
+/**
+Точка входа в программу, для вывода на консоль надписи "Hellow world"
+*/
  public static void main (String[] args)
-	 {
+	 {	
+	 /**
+	Метод System.out.println 
+	@param name Kretov Alexandr
+	@return - Выводит в консоль "Hello world"
+	*/
 		 System.out.println ("Hello world");
 	 } 
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Kretov Alexandr (no mail)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
