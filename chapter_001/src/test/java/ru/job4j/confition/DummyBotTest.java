3690104355ef2c0d4f1dd40ae377b0a3f9dd8dd7
Kretov Alexandr <tasolcheg@gmail.com>
2018-06-21 22:54:37 +0300

    4.1. Глупый бот.

---
+++ chapter_001/src/test/java/ru/job4j/confition/DummyBotTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.confition;
+
+import org.junit.Test;
+import ru.job4j.condition.DummyBot;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author or Kretov Alexandr (mailto:no mail)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class DummyBotTest {
+    @Test
+    public void WhenGreetBot () {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот"),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByuBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите другой вопрос.")
+        );
+    }
+}
