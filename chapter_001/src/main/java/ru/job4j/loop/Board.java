6284bfeb0e1d552c2571b73b5e5aa8e9563e2f42
Kretov Alexandr <tasolcheg@gmail.com>
2018-07-17 22:31:53 +0300

    5.3. Построить шахматную доску в псевдографике.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Board {
+    public String paint (int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int x = 0; x < height; x++) {
+            for (int i = 0; i < width; i++) {
+                //Условия проверки когда нужно писать X
+                if ((x + i) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import ru.job4j.loop.Board;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author or Kretov Alexandr (mailto:no mail)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BoardTest {
+    @Test
+    public void when3x3() {
+        Board board = new Board();
+        String rsl = board.paint(3, 3);
+        String ln = System.lineSeparator();
+        assertThat(rsl, is(
+                String.format("X X%s X %sX X%s", ln, ln, ln)
+                )
+        );
+    }
+}
