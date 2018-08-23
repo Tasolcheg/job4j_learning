dd29d30053a88061cd4bdab781c9b3495acec690
Кретов Александр Александрович <a.kretov@hq.qiwi.com>
2018-08-23 12:47:53 +0300

    Правка

--- chapter_002/src/main/java/ru.job4j/profession/Doctor.java
+++ chapter_002/src/main/java/ru.job4j/profession/Doctor.java
@@ -3,6 +3,7 @@ package ru.job4j.profession;
 public class Doctor extends Profession {
 
     public Diagnose heal(Patient pacient) {
+        return null;
     }
 
     public Doctor(String name, String profession) {
