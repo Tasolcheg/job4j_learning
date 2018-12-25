0be27bb162cd9d2d16e4a43fcba2824ff890f4c1
Кретов Александр Александрович <a.kretov@hq.qiwi.com>
2018-12-25 16:10:31 +0300

    правки теста

--- chapter_002/src/test/java/ru/jobj/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/jobj/tracker/ValidateInputTest.java
@@ -45,4 +45,18 @@ public class ValidateInputTest {
                 )
         );
     }
+
+    @Test
+    public void whenInvalidInput7() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"7", "1"})
+        );
+        input.ask("Enter", new int[] {1});
+        assertThat(
+                this.mem.toString(),
+                is(
+                        String.format("Неверный выбор пункта меню2%n")
+                )
+        );
+    }
 }
